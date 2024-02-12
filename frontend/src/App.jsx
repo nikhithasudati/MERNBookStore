8e423814f8659cdd99d38f15ae9ab19144f33d89
Nikhitha Sudati <nikhithasudati78@gmail.com>
2024-02-12 17:27:54 -0500

    frontend initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Routes,Route} from 'react-router-dom';
+import CreateBook from './pages/CreateBook';
+import DeleteBook from './pages/DeleteBook';
+import EditBook from './pages/EditBook';
+import Home from './pages/Home';
+import ShowBook from './pages/ShowBook';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path = '/' element= {<Home />} />
+      <Route path = '/books/create' element= {<CreateBook/>} />
+      <Route path = '/books/delete' element= {<DeleteBook />} />
+      <Route path = '/books/edit' element= {<EditBook />} />
+      <Route path = '/books/show' element= {<ShowBook />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
